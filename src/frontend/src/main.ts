6a7df3c2ab2cb5b053fad6eecc66a080eb496b70
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2026-07-04 19:44:05 -0500

    build(frontend): add tailwind pkgs

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { PrimeVue } from '@primevue/core'
 import Aura from '@primeuix/themes/aura'
+import '@/assets/main.css'
 
 const app = createApp(App)
 
