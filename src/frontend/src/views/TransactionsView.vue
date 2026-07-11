b7322406bf9922e953aae8d6f1cf9edeb7abc3f3
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2026-07-10 21:30:35 -0500

    feat(frontend): set sticky position for bottom bar

--- src/frontend/src/views/TransactionsView.vue
+++ src/frontend/src/views/TransactionsView.vue
@@ -18,7 +18,7 @@ function openDialog() {
   <div class="flex gap-4 h-full">
     <div class="flex flex-col flex-1 gap-4 max-w-full">
       <TransactionsList :expenses="store.expenses" class="flex-1" />
-      <TransactionsBottomBar @add="openDialog" />
+      <TransactionsBottomBar @add="openDialog" class="sticky bottom-0" />
     </div>
   </div>
 
