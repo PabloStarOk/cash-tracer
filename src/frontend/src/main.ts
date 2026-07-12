610938e723281af7a652de67c10c85c7c8fedc84
PabloStarOk <pedropablorodriguezcarranza@gmail.com>
2026-07-11 22:53:50 -0500

    feat(frontend): organize primevue config and change primary color to cyan

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -2,18 +2,14 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import { PrimeVue } from '@primevue/core'
-import Aura from '@primeuix/themes/aura'
 import '@/assets/main.css'
 import { ConfirmationService, ToastService } from 'primevue'
+import { primeVueConfig } from '@/plugins/primevue'
 
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura,
-  },
-})
+app.use(PrimeVue, primeVueConfig)
 app.use(ConfirmationService)
 app.use(ToastService)
 
